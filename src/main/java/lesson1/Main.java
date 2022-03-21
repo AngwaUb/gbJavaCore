f6a774ec0f755a1c5031d31fad0aa66b0b2d8adf
IrinaK <irina_koryakina@mail.ru>
2022-03-21 21:21:20 +0500

    Lesson1

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,27 @@
+package lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        Team dreamTeam = new Team("Сборная мира",
+                new Athlete("Иванов", 20, 8, 25),
+                new Athlete("Смит", 22, 2.5, 23),
+                new Athlete("Чи", 19.6, 2.9, 22),
+                new Athlete("Сорос", 19, 5, 16.9)
+        );
+        Course stadium = new Course("Динамо",
+                new Track("Беговая дорожка", 500, 2),
+                new Pool("Большая ванна", 50, 10),
+                new SkiTrack("Лыжня", 2000)
+        );
+
+        dreamTeam.print();
+
+        System.out.println();
+
+        stadium.doIt(dreamTeam);
+
+        System.out.println("Результат:");
+
+        dreamTeam.print();
+    }
+}
