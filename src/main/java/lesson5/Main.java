a80038be575ad507fa48b99fb20e583557db5137
IrinaK <irina_koryakina@mail.ru>
2022-04-09 10:55:35 +0500

    Lesson5

---
+++ src/main/java/lesson5/Main.java
@@ -0,0 +1,61 @@
+package lesson5;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] header = {"English", "Русский", "日本語"};
+        int[][] data = {{100, 200, 300}, {40, 50, 60}};
+        AppData appData = new AppData(header, data);
+        System.out.println(appData);
+        save(appData);
+        AppData loadingData = load();
+        System.out.println(loadingData);
+    }
+
+    private static AppData load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("appdata.csv"))) {
+            String[] header = reader.readLine().split(";");
+
+            ArrayList<String> lines = new ArrayList<>();
+
+            String nextline;
+            while ((nextline = reader.readLine()) != null) {
+                lines.add(nextline);
+            }
+            int[][] data = new int[lines.size()][header.length];
+            for (int i = 0; i < lines.size(); i++) {
+                String[] splitLine = lines.get(i).split(";");
+                for (int j = 0; j < splitLine.length; j++) {
+                    data[i][j] = Integer.parseInt(splitLine[j]);
+                }
+            }
+            return new AppData(header, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void save(AppData appData) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("appdata.csv"))) {
+            writer.write(appData.getHeader()[0]);
+            for (int i = 1; i < appData.cols(); i++) {
+                writer.write(";" + appData.getHeader()[i]);
+            }
+            writer.newLine();
+            for (int i = 0; i < appData.rows(); i++) {
+                writer.write("" + appData.getData()[i][0]);
+                for (int j = 1; j < appData.cols(); j++) {
+                    writer.write(";" + appData.getData()[i][j]);
+                }
+                if (i < appData.rows() - 1) {
+                    writer.newLine();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
